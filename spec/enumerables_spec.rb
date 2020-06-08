db2e26012a4aa161a3b0c454a314954f0346b736
paulo-techie <paulo.techie@gmail.com>
2020-06-08 14:44:17 -0600

    Add my_each_with_index and my_select tests

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -19,4 +19,32 @@ describe Enumerable do
       expect(test_result).to eql(expected_result)
     end
   end
+
+  describe "#my_each_with_index" do
+    it "returns to enum if no block is provided" do
+    expect(arr_num.my_each_with_index).to be_a(Enumerator)
+    end
+  
+    it "returns a new array with the result of the operation within the block" do
+      expected_result = [[1,0],[2,1],[3,2],[4,3],[5,4]]
+      test_result = []
+      arr_num.my_each_with_index { |item, index| test_result << [item, index]}
+      expect(test_result).to eql(expected_result)
+    end
+  end
+
+  describe "#my_select" do
+    it "returns to enum if no block is provided" do
+    expect(arr_num.my_select).to be_a(Enumerator)
+    end
+  
+    it "returns a new array with the result of the operation within the block" do
+      expected_result = [2,4]
+      test_result = []
+      arr_num.my_select { |item| test_result << item if(item.even?)}
+      expect(test_result).to eql(expected_result)
+    end
+  end
+
+
 end
